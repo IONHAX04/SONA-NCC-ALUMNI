09fc0bc8fc664843d6a2ec0a8f065bab3e9d6ea0
ionhax-dev <thirukumara.d@zadroit.com>
2025-09-28 12:28:39 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ncc-alumni',
+  webDir: 'dist'
+};
+
+export default config;
